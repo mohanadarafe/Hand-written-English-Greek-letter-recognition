d806b230f4c000c373353f333898ec926808b9e1
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-10-14 01:36:42 -0400

    Add a jupyter notebook for basic data exploration

--- gaussian.py
+++ gaussian.py
@@ -1,6 +1,6 @@
 import preprocess
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
+import sklearn
 
 english_training_filename = './data/Assig1-Dataset/train_1.csv'
 english_test_filename = './data/Assig1-Dataset/test_with_label_1.csv'
@@ -8,14 +8,14 @@ english_validate_filename = './data/Assig1-Dataset/val_1.csv'
 
 greek_training_filename = '/data/Assig1-Dataset/train_2.csv'
 
-clf = GaussianNB()
+clf = sklearn.naive_bayes.GaussianNB()
 X_train, Y_train = preprocess.convertFileToTrainingSet(english_training_filename)
 
 X_val, Y_val = preprocess.convertFileToTrainingSet(english_training_filename)
 
-model = clf.fit(X_train,Y_train)
-
 X_test, Y_test = preprocess.convertFileToTrainingSet(english_test_filename)
+
+model = clf.fit(X_train,Y_train)
 
 train_acc  = model.score(X_train, Y_train)
 val_acc = model.score(X_val, Y_val)
@@ -24,6 +24,7 @@ test_acc= model.score(X_test,Y_test)
 print(f'train accuracy is {train_acc}')
 print(f'validation accuracy is {val_acc}')
 print(f'test accuracy is {test_acc} ')
+
 
 
 
